f352bf447590d1a6339248f143b7493a276c5fca
Fully34 <cfullinw34@gmail.com>
2015-06-30 15:31:25 -0600

    finished removeM with Morgans help

--- script.js
+++ script.js
@@ -57,3 +57,46 @@ function keyValue(key, value) {
 //===========================================================================//
                         /* ~~~ # 4 ~~~ */ 
 //===========================================================================//
+
+function negativeIndex(array, negIndex) {
+
+    return array[ array.length + negIndex ];
+}
+
+negativeIndex(['a', 'b', 'c', 'd', 'e'], -2);
+negativeIndex(['jerry', 'sarah', 'sally'], -1);
+
+
+//===========================================================================//
+                        /* ~~~ # 5 ~~~ */ 
+//===========================================================================//
+
+//This one can only deal with 1 'm/M' in a row
+
+// function removeM(string) {
+
+//     var arr = string.split('');
+
+//     for (var i = 0; i < arr.length; i++) {
+        
+//         if ( arr[i] === "m" || arr[i] === "M") {
+
+//             arr.splice(i, 1);
+//         }
+//     }
+
+//     return arr.join('');
+// }
+
+// How about using some freaking built in functions like an actual real, live programmer?
+
+function removeM(string) {
+
+    return string.toLowerCase().split('').filter(function(l) {
+
+        return l !== 'm';
+
+    }).join('');
+
+}
+
