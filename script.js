8ed30bae8a53ca91e2cadc889db11c8451252619
Fully34 <cfullinw34@gmail.com>
2015-06-30 16:29:00 -0600

    finished commenting all my answers

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 
 function getName(obj) {
 
+    // super simple object value retrieval
     return obj.name;
 }
 
@@ -25,10 +26,13 @@ function getName(obj) {
 
 function totalLetters(arr) {
 
+    // Set count variable to keep track of how many characters we come across
     var count = 0;
 
+    // Loop thru array
     for (var i = 0; i < arr.length; i++) {
         
+        // add the character count of each element to "count" variable
         count += arr[i].length;
     };
 
@@ -60,12 +64,14 @@ function keyValue(key, value) {
 
 function negativeIndex(array, negIndex) {
 
+    // checking the array element that is negIndex from the end of the array
     return array[ array.length + negIndex ];
 }
 
 negativeIndex(['a', 'b', 'c', 'd', 'e'], -2);
 negativeIndex(['jerry', 'sarah', 'sally'], -1);
 
+// -> Victory
 
 //===========================================================================//
                         /* ~~~ # 5 ~~~ */ 
@@ -92,30 +98,35 @@ negativeIndex(['jerry', 'sarah', 'sally'], -1);
 
 function removeM(string) {
 
+    // make string all lowercase letters -> split into array -> filter that array by asking if each element is not equal to 'm' -> .filter(); returns an array containing all elements who were not equal to 'm'
     return string.toLowerCase().split('').filter(function(l) {
 
         return l !== 'm';
 
-    }).join('');
+    }).join(''); // -> Join the array back into a string
 
 }
 
+// -> Victory
 
 //===========================================================================//
                         /* ~~~ # 6 ~~~ */ 
 //===========================================================================//
 
-var myObject = { a: 10, b: 20, c: 30 };
-var mySecondObject = { firstName: 'pork', lastName: 'chops' };
+// var myObject = { a: 10, b: 20, c: 30 };
+// var mySecondObject = { firstName: 'pork', lastName: 'chops' };
+
 
 function printObject(obj) {
 
+    // Simple shit
     for (key in obj) {
 
         console.log(key + " : " + obj[key])
     }
 }
 
+// -> Victory
 
 //===========================================================================//
                         /* ~~~ # 7 ~~~ */ 
@@ -123,17 +134,17 @@ function printObject(obj) {
 
 function vowels(string) {
 
+    //create vowels array
     var vowels = 'aeiouy'.split('');
 
-    var container = [];
-
+    // Filtering an inline array and checking if each element is in the vowels array 
     return string.toLowerCase().split('').filter(function(a){
 
-        return ( vowels.indexOf(a) > - 1 );
+        return ( vowels.indexOf(a) > - 1 ); // -> if this eval's to true, it means that the element is in the vowels array and will be returned by the .filter(); method
     })
 }
 
-
+// -> Victory
 
 //===========================================================================//
                         /* ~~~ # 8 ~~~ */ 
@@ -147,23 +158,29 @@ function vowels(string) {
 
 function twins(arr) {
 
-
+    // return false immediately if the array has an odd length
     if (arr.length % 2 !== 0) {
 
         return false;
     }
 
+    // Loop thru array starting at 0th element 
     for (var i = 0; i < arr.length; i += 2) {
 
         if ( arr[i] !== arr[i + 1] ) {
 
+            // if the element is not equal to subsequent element, we stop looping
             return false;
         }
+
+        //If the element is equal to the subsequent element, we increment the index by 2 to skip over the subsequent element
     }
 
+    // If we don't get proven false anywyere else, we can assum that we have an array of twins
     return true;
 }
 
+// -> Victory
 
 //===========================================================================//
                         /* ~~~ # 9 ~~~ */ 
@@ -176,11 +193,13 @@ function twins(arr) {
 
 function or(arr) {
 
+    // return false immediately if array doesn't have any contents
     if ( arr.length === 0) {
 
         return false;
     }
 
+    // loop thru array
     for (var i = 0; i < arr.length; i++) {
 
         if ( arr[i] ) {
@@ -192,6 +211,7 @@ function or(arr) {
     return false;
 }
 
+// -> Victory
 
 //===========================================================================//
                         /* ~~~ # 10 ~~~ */ 
@@ -207,12 +227,13 @@ function unique(arr) {
 
     var uniqueArr = [];
 
-    // Loop thru array
+    // Loop thru array, add each value as a key in unique object
     for (var i = 0; i < arr.length; i++) {
         
         uniqueObj[ arr[i] ] = null;
     }
 
+    // Loop thru object -> push each key to uniqueArr
     for (key in uniqueObj) {
 
         uniqueArr.push(key);
@@ -221,3 +242,4 @@ function unique(arr) {
     return uniqueArr;
 }
 
+// -> Victory
